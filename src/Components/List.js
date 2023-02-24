d33eea178fc4886caddcea0d0d895449945a8523
shahrookhansahil <shahroo.bscsf19@iba-suk.edu.pk>
2023-02-24 23:08:58 +0500

    first commit

---
+++ src/Components/List.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div className='list'>
+        <h4>Name</h4>
+        <h4>Email</h4>
+        <h4>Remove</h4>
+    </div>
+  )
+}
+
+export default List
